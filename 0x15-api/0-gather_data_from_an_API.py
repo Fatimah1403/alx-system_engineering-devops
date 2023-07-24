56ccf8454206bbc5ee7c592d8bcfe8ee551b825a
Fatimah1403 <fattyhassan14@gmail.com>
2023-07-24 21:12:11 +0000

    Task 0 updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/"
-    user_id = req.get(url + 'users/{}'.faormat(sys.argv[1])).json()
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     to_dos = req.get(url + 'todos', params={'userId': sys.argv[1]}).json()
 
     # print(to_do)
