c4556e01b057e9cac93ec933047245c90c235bab
Fatimah1403 <fattyhassan14@gmail.com>
2023-07-24 22:56:11 +0000

    Task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Exporting the employer to_do list in json format
+"""
+import json
+import requests as req
+import sys
+
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = req.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = req.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as json_file:
+        json.dump({user_id: [{
+                "task": el.get("title"),
+                "completed": el.get("completed"),
+                "username": "username"
+                } for el in todos]}, json_file)
