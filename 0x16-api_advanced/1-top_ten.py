a588844e0773329607cf873d578cd11bedad6c35
Fatimah1403 <fattyhassan14@gmail.com>
2023-08-08 22:21:04 +0000

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,6 +4,7 @@ hotposts listed for a given subreddit"""
 
 import requests
 
+
 def top_ten(subreddit):
     """A function that prints the titles of the
     first 10 hot posts listed for a given subreddit.
@@ -15,4 +16,18 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot".format(subreddit)
     first_ten = {"limit": 10}
 
-    headers = 
+    headers = {
+            "User-Agent": 'Google chrome version 115.0.5790.171'
+    }
+
+    response = get(url, headers=headers first_ten=first_ten,
+                   allow_redirects=False)
+    result = request.json()
+
+    if response.status_code == 404:
+        print("None")
+        return
+    my_data = result.get('data').get('children')
+
+    for d in my_data:
+        print(d.get('data').get('title'))
