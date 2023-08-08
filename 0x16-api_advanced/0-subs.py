c85b7d1eb2bbf1874b3b5a58a5ed4498142b13b7
Fatimah1403 <fattyhassan14@gmail.com>
2023-08-08 20:20:57 +0000

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" querries that retuens Reddit api, the total nos of subscribers"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """a function that queries the Reddit API and returns
+    the number of subscribers(not active users, total subscribers)
+    for a given subreddit. If an invalid subreddit is given,
+    the function should return 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    headers = {
+            "User-Agent": 'Google chrome version 115.0.5790.171'
+            }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status.code == 404:
+        return 0
+    result = response.json().get("data")
+    return result.get("subscribers")
