b59d167f4593abc18006a8ce52510023db1a995e
Fatimah1403 <fattyhassan14@gmail.com>
2023-08-09 01:16:21 +0000

    Task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,8 +8,25 @@ are found for the given subreddit, the function should return None.
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
-     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
+    param = {"after": after,
+             "count": count,
+             "limit": 100
+             }
+    request = requests.get(url, headers=headers, param=param,
+                           allow_redirects=False)
+    if response.status_code == 404:
+        return None
+    results = response.json().get('data')
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get.("title"))
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
